0008cefd7c9a3aba8535ccc314fe6158144e663c
vanagila <vanagilakedna@gmail.com>
2022-05-18 17:34:01 -0300

    style(css): reset, fonts and globalstyle using styled-component

---
+++ src/styles/global.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  :root {
+    /* primary-palette */
+    --primary: #ff577f;
+    --primary-focus: #ff427f;
+    --primary-negative: #59323f
+    
+    /* grey scale palette */
+    --grey-0: #f8f9fa
+    --grey-1: #868e96;
+    --grey-2: #343b41;
+    --grey-3: #212529;
+    --grey-4: #121214;
+
+    /* feedback palette */
+    --sucess: #3fe864;
+    --negative: #e83f5b;
+  }
+
+  body, input, button, h1, h2, h3, h4, h5, h6, p, span {
+    background: var(--grey-4);
+    font-family: 'Inter', sans-serif;
+  }
+
+`;
